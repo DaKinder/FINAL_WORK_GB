bd4936a0c9943b9e0d7a9267686081c5c6eddf98
DaKinder <103947080+DaKinder@users.noreply.github.com>
2023-04-18 15:30:55 +0300

    add program code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateArray(int length)
+{
+    string[] arr = new string[length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите слово: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+int CouuntElements(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i].Length <= 3) count++;
+    return count;
+}
+
+void PrintNewArr(string[] arr, int count)
+{
+    string[] newArr = new string[count];
+    for (int i = 0, j = 0; i < arr.Length; i++)
+        if (arr[i].Length <= 3)
+        {
+            newArr[j] = arr[i];
+            Console.Write($"|{newArr[j]}| ");
+            j++;
+        }
+}
+
+Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+string[] arr = CreateArray(length);
+PrintNewArr(arr, CouuntElements(arr));
